6616e73d115a02f066d5cc6a12b6b76ef090bddc
elpatron1133 <elpatron59093@gmail.com>
2022-08-27 14:15:42 -0500

    v0.2 se mejoro la busqueda ahora muestra todas las cartas que coicida

--- findCard.js
+++ findCard.js
@@ -1,5 +1,4 @@
 const URL = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
-fetch;
 fetch(URL)
   .then((res) => res.json())
   .then((data) => {
@@ -8,26 +7,49 @@ fetch(URL)
     const buscar = () => {
       const busquedaUser = document.getElementById("input").value;
       const busquedaMin = busquedaUser.toLowerCase();
-      console.log(busquedaUser)
-      
-     
+      console.log(busquedaUser);
+
       const palabras = busquedaMin.split(" ");
 
-      let busqueda = palabras.map((palabra) => {
-           
-    return palabra[0].toUpperCase() + palabra.substring(1); 
-      }).join(" ");
+      let busqueda = palabras
+        .map((palabra) => {
+          return palabra[0].toUpperCase() + palabra.substring(1);
+        })
+        .join(" ");
 
-      
-
-      const resultado = array.find((item) => item.name.includes(busqueda));
+      const resultado = array.filter((item) => item.name.includes(busqueda));
       console.log(resultado);
-      let img = resultado.card_images[0].image_url;
-      let mostrarImagen = document.getElementById("img");
-      mostrarImagen.src = img;
+      resultado.forEach((item, index) => {
+        const name = item.name;
+        const img = document.createElement('img');
+        img.src = item.card_images[0].image_url
+        const card = document.createElement("div")
+        card.className = "img-item"
+        card.innerHTML = `<h3>${name}</h3> <img src="${img.src}" alt="${name}">`
+
+        document.getElementById("resultado").append(card)
+
+
+      });
     };
+    function limpiarBusqueda(){
+      const resultado = document.getElementById("resultado");
+      resultado.innerHTML = "";
+    }
+
+
+
+
     const btn = document.getElementById("btn");
+    input.addEventListener("keypress",(e)=>{
+      if(e.key === "Enter"){
+        limpiarBusqueda();
+          buscar();
+      }
+    })
+
     btn.addEventListener("click", () => {
+      limpiarBusqueda();  
       buscar();
     });
   })
